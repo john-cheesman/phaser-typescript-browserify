0f01a8a5335e3fb268e67644a9e4789e7cdeb720
John Cheesman <me@johncheesman.org.uk>
2016-11-17 23:44:50 +0000

    Add Phaser and build process

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,7 @@
+/// <reference path="../../node_modules/phaser/typescript/phaser.d.ts" />
 import Player from './models/player'
 
 let player = new Player(`John`, `Cheesman`);
+let game = new Phaser.Game();
 
 console.log(player, player.fullName);
